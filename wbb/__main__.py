d2f89413e0cef3232ced6d0c4adac9e093b25ed7
Vijay <95028968+AttitudeKing-vj@users.noreply.github.com>
2022-06-15 20:41:34 +0530

    Update __main__.py

--- wbb/__main__.py
+++ wbb/__main__.py
@@ -133,7 +133,7 @@ home_keyboard_pm = InlineKeyboardMarkup(
         ],
         [
             InlineKeyboardButton(
-                text="Add Me To Your Group ",
+                text="✚ Add Me To Your Group ✚",
                 url=f"http://t.me/{BOT_USERNAME}?startgroup=new",
             )
         ],
